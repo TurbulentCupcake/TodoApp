80367282d3a705dd3a957be4238466aecfe8b4f9
Matthew Wolff <mm.wolff@chi-squared.org>
2018-03-13 02:50:23 -0500

    removed key?

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q=1a13fixz+!yaw$zo@w&@+1rwii$qf6c9#gqteo-c5qf&leqa'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
