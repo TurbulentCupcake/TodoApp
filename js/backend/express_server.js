b679b00e5dfb8f4170981f18d311d5d5511168db
TurbulentCupcake <muraliadithya315@gmail.com>
2018-03-27 19:05:04 -0500

    made a webs erver that hosts the page using express js

---
+++ js/backend/express_server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+host = 'localhost';
+port = 8000;
+
+app.use(express.json()); // activate json parsing 
+app.use(express.static('/home/adithya/Projects/TodoApp/js/scripts'));
+app.use(express.static('/home/adithya/Projects/TodoApp/js/views'))
+
+// render the initial page
+app.get('/',(req, res)=>{
+	res.sendFile('index.html');
+})
+
+
+
+app.listen(port);
+
+console.log("Running at port " + port);
+
+
+/*// render the css and js page
+app.get('/todo.js', (req, res)=>{
+	res.sendFile('../frontend/todo.js');
+})
+
+// rend the css file
+app.get('/todo.css', (req,res)=>{
+	res.sendFile('../frontend/todo.css');
+})
+
+*/
